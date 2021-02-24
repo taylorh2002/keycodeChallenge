a279cc1b567972259e3ca926baa9e09a6e9d769e
taylorh2002 <taylor.headford@resurgem.co.uk>
2021-02-24 09:53:17 +0000

    Second commit

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ function myFunction(event) {
     let x = event.which || event.keyCode;
     document.getElementById("eventWhich").innerHTML = "event.which is: " + x;
 
-    let y = document.getElementById("input");
-    document.getElementById("eventCode").innerHTML = "event.code is: " + y;
-  }
+    var y = String.fromCharCode(x);
+    document.getElementById("eventKey").innerHTML = "event.key is: " + y;
+
+    var z = event.code;
+    document.getElementById("eventCode").innerHTML = "event.code is: " + z;
+  } 
